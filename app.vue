2b3300213c3820ce842588253f1d41887b57fa64
andyjjrt <andyjjrt@gmail.com>
2022-07-01 10:58:47 +0800

    fix: Change padding in different viewport

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <Navbar />
-    <div class="container mx-auto p-10">
+    <div class="container mx-auto p-10 lg:px-36 md:px-28">
       <NuxtPage />
     </div>
+    <Footer />
   </div>
 </template>
